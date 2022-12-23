7320ef62306573bb99710e18a12ed8f26027f269
Gvantsa Okropiridze <oqropiridzegvantsa@gmail.com>
2022-12-23 19:53:46 +0400

    TCP-IP-Server-Client-Communication

---
+++ ConsoleApplication/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public partial class Client
+    {
+        TcpClient tcpClient = new TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readdata = null;
+
+        public void Connect1(string host, int port)
+        {
+
+            tcpClient.Connect(host, port);
+
+            Thread ct = new Thread(getMessage);
+            ct.Start();
+        }
+
+        private void getMessage()
+        {
+            string returnData;
+            while (true)
+            {
+                serverStream = tcpClient.GetStream();
+                var buffSize = tcpClient.ReceiveBufferSize;
+                byte[] inStream = new byte[buffSize];
+                serverStream.Read(inStream, 0, buffSize);
+
+                returnData = Encoding.ASCII.GetString(inStream);
+
+                readdata = returnData;
+                Console.WriteLine(readdata);
+                sendMessage();
+            }
+
+
+        }
+        private void sendMessage()
+        {
+            var text = Console.ReadLine();
+            byte[] outStream = Encoding.ASCII.GetBytes(text);
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+        }
+    }
+}
---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO.Ports;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using ConsoleApplication;
+
+Client client = new Client();
+client.Connect1("127.0.0.1", 23);
+Console.ReadLine();
+
